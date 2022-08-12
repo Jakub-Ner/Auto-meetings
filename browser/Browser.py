bb9c2a0853970f3e15058ed494eef6a69b72a449
jakub-ner <kubaner1@gmail.com>
2022-08-12 20:07:04 +0200

    refactoring and bugfixing

--- browser/Browser.py
+++ browser/Browser.py
@@ -13,22 +13,12 @@ class Browser:
         with open(VARIABLES_PATH + "meetings.json", "w+") as file:
             # if file is empty give necessary data
             if os.stat(VARIABLES_PATH + "meetings.json").st_size == 0:
-                necessary_data = {
-                    "@1234": {
-                        "date": "40-13-1900 25:61",  # DD-MM-YYYY hh:mm
-                        "link": ""
-                    }}
-                json.dump(necessary_data, file, indent=2)
-                self.meetings = necessary_data
+                json.dump({}, file, indent=2)
+                self.__meetings = {}
 
             else:
                 with open(VARIABLES_PATH + "meetings.json", "r") as data:
-                    self.meetings = json.load(data)
-
-    @staticmethod
-    def __create_key(name, index):
-        key = name.split('@')
-        return key[0] + key[1] + str(index)
+                    self.__meetings = json.load(data)
 
     def search_meetings_periodically(self):
         while True:
@@ -39,43 +29,49 @@ class Browser:
         logging.info('echo start searching')
         disposable_meetings = {}
 
-        mail = Mail()
-        if not mail.log_in():
+        mailbox = Mail()
+        if not mailbox.log_in():
             return
 
         try:
-            mail.mail.select('inbox')
+            for email in self.get_emails():
+                urls, dates = mailbox.read_mails(email)
+                self.__meetings[email]["link"] = urls[0]
+                self.__meetings[email]["date"] = dates[0]
+                if dates:
+                    dates[0][1] = convert_months_to_numbers(dates[0][1])  # e.g "lutego" into 2
 
-            for mail in self.meetings:
-                if len(mail) > 1:
-                    if "@" in mail:
-                        urls, dates = mail.read_mails(mail)
-                        self.meetings[mail]["link"] = urls[0]
-                        self.meetings[mail]["date"] = dates[0]
-                        if dates:
-                            dates[0][1] = convert_months_to_numbers(dates[0][1])  # e.g "lutego" into 2
+                # add disposable meetings from all messages from email
+                self.__meetings[email]["meetings"] = {}
+                for i in range(1, len(urls)):
+                    # key = str(email) + str(i)
+                    key = create_key(email, i)
+                    if dates:
+                        dates[i][1] = convert_months_to_numbers(dates[i][1])
+                    disposable_dict = {key: {"link": urls[i], "date": dates[i]}}
+                    disposable_meetings.update(disposable_dict)
 
-                        # add disposable meetings from all messages from mail
-                        self.meetings[mail]["meetings"] = {}
-                        for i in range(1, len(urls)):
-                            # key = str(mail) + str(i)
-                            key = self.__create_key(mail, i)
-                            if dates:
-                                dates[i][1] = convert_months_to_numbers(dates[i][1])
-                            disposable_dict = {key: {"link": urls[i], "date": dates[i]}}
-                            disposable_meetings.update(disposable_dict)
+            # new mails will be added to the beginning of the list,
+            # disposable links are added at the end, so If we meet one, we can finish the loop
 
-                    # new mails will be added to the beginning of the list,
-                    # disposable links are added at the end, so If we meet one, we can finish the loop
-                    else:
-                        break
-
-            mail.end()
+            mailbox.log_out()
         except:
             logging.error('echo "ERROR: Lack of internet connection!"')
 
-        self.meetings.update(disposable_meetings)
-        prepare_next_meeting(self.meetings)
+        self.__meetings.update(disposable_meetings)
+        prepare_next_meeting(self.__meetings)
 
         with open(VARIABLES_PATH + "meetings.json", "w") as data:
-            json.dump(self.meetings, data, indent=2)
+            json.dump(self.__meetings, data, indent=2)
+
+    def get_emails(self):
+        emails = []
+        for name in self.__meetings:
+            if '@' in name and name not in emails:
+                emails.append(name[:-4])
+        return emails
+
+
+def create_key(name, index):
+    key = name.split('@')
+    return key[0] + key[1] + str(index)
--- browser/Mail.py
+++ browser/Mail.py
@@ -7,36 +7,42 @@ import email
 
 class Mail:
     def __init__(self):
-        self.SMTP_PORT = 993
-        self.SMTP_SERVER = "imap.gmail.com"
-        self.mail = imaplib.IMAP4_SSL(self.SMTP_SERVER)
-        self.email_from = ""
+        self.__SMTP_PORT = 993
+        self.__SMTP_SERVER = "imap.gmail.com"
+        self.__mail = imaplib.IMAP4_SSL(self.__SMTP_SERVER)
+        self.__sender = ""
 
     def log_in(self):
         if MY_MAIL and PASS:
-            self.mail.login(MY_MAIL, PASS)
+            self.__mail.login(MY_MAIL, PASS)
+            self.__mail.select('inbox')
             return True
         return False
 
     def read_mails(self, email_from):
-        self.email_from = email_from
-        data = self.mail.search(None, 'FROM', self.email_from)
+        self.__sender = email_from
+        data = self.__mail.search(None, 'FROM', self.__sender)
         ids = data[1][0].split()
         ids.reverse()
 
         links = []
         dates = []
         for i in ids:
-            data = self.mail.fetch(str(int(i)), '(RFC822)')
+            data = self.__mail.fetch(str(int(i)), '(RFC822)')
             state, response_part = data
             message = str(email.message_from_string(str(response_part[0][1], "utf-8")).get_payload(0))
-            link, date = self.find_data(message)
+            link, date = self.__find_data(message)
             if link:
                 links.append(link)
+
                 dates.append(date)
         return links, dates
 
-    def find_data(self, message):
+    def log_out(self):
+        self.__mail.close()
+        self.__mail.logout()
+
+    def __find_data(self, message):
         words = message.split()
         for i, word in enumerate(words):
             if "termin" in word:
@@ -54,10 +60,6 @@ class Mail:
                             return words[i], date
 
                 except:
-                    logging.error(f'echo "ERROR: Invalid date format in mail! from {self.email_from}"')
+                    logging.error(f'echo "ERROR: Invalid date format in mail! from {self.__sender}"')
 
         return "", []
-
-    def end(self):
-        self.mail.close()
-        self.mail.logout()
