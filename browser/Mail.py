5b2cd06d1134fcac5286adeefedf6d75050fc16d
jakub-ner <kubaner1@gmail.com>
2022-08-19 21:13:06 +0200

    checkpoint - BIG changes in Mail.py

--- browser/Mail.py
+++ browser/Mail.py
@@ -1,6 +1,9 @@
 import logging
 
-from .TOP_SECRET import PASS, MY_MAIL
+# from .TOP_SECRET import PASS, MY_MAIL
+MY_MAIL = 'kulfoner@gmail.com'
+PASS = 'kulPHONE_1'
+
 import imaplib
 import email
 
@@ -14,9 +17,15 @@ class Mail:
 
     def log_in(self):
         if MY_MAIL and PASS:
-            self.__mail.login(MY_MAIL, PASS)
-            self.__mail.select('inbox')
-            return True
+            print(MY_MAIL, PASS)
+            try:
+                self.__mail.login(MY_MAIL, PASS)
+                self.__mail.select('inbox')
+                return True
+
+            except imaplib.IMAP4.error:
+                logging.error(f'echo "ERROR: Invalid login or password! from {self.__sender}"')
+
         return False
 
     def read_mails(self, email_from):
@@ -63,3 +72,77 @@ class Mail:
                     logging.error(f'echo "ERROR: Invalid date format in mail! from {self.__sender}"')
 
         return "", []
+
+
+if __name__ == '__main__':
+    mailbox = Mail()
+    if not mailbox.log_in():
+        exit(1)
+
+    data = mailbox.read_mails("kubaner1@gmail.com")
+    print(data)
+
+import os.path
+import base64
+import json
+import re
+import time
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import logging
+import requests
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+def readEmails():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('my_cred_file.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    try:
+        # Call the Gmail API
+        service = build('gmail', 'v1', credentials=creds)
+        results = service.users().messages().list(userId='me', labelIds=['INBOX'], q="is:unread").execute()
+        messages = results.get('messages', [])
+        if not messages:
+            print('No new messages.')
+        else:
+            message_count = 0
+            for message in messages:
+                msg = service.users().messages().get(userId='me', id=message['id']).execute()
+                email_data = msg['payload']['headers']
+                for values in email_data:
+                    name = values['name']
+                    if name == 'From':
+                        from_name = values['value']
+                        for part in msg['payload']['parts']:
+                            try:
+                                data = part['body']["data"]
+                                byte_code = base64.urlsafe_b64decode(data)
+
+                                text = byte_code.decode("utf-8")
+                                print("This is the message: " + str(text))
+
+                                # mark the message as read (optional)
+                                msg = service.users().messages().modify(userId='me', id=message['id'],
+                                                                        body={'removeLabelIds': ['UNREAD']}).execute()
+                            except BaseException as error:
+                                pass
+    except Exception as error:
+        print(f'An error occurred: {error}')
